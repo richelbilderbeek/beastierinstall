4ad2f4f13f1e2de0da13d4efa516c86c846d0022
richelbilderbeek <richel@richelbilderbeek.nl>
2021-05-22 10:38:35 +0200

    Fix script

--- scripts/install_beast2.sh
+++ scripts/install_beast2.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #
-# Installs BEAST2 using the R code in the 'beastier' package
+# Installs BEAST2 using the R code in the 'beastierinstall' package
 #
-# Because the R code in the package uses 'beastier::' in calling
+# Because the R code in the package uses 'beastierinstall::' in calling
 # its functions, this scipt 
 #
 # Usage:
@@ -10,11 +10,8 @@
 #  ./scripts/install_beast2.sh
 #
 #
-cat R/install_beast2.R > install_beast2.R
-cat R/check_os.R >> install_beast2.R
-cat R/get_default_beast2_download_url.R >> install_beast2.R
-cat R/get_default_beast2_version.R >> install_beast2.R
+cat R/*.R > install_beast2.R
 echo "install_beast2()" >> install_beast2.R
-sed -i'.orginal' "s/beastier:://g" install_beast2.R
+sed -i'.orginal' "s/beastierinstall:://g" install_beast2.R
 cat install_beast2.R
 Rscript install_beast2.R
