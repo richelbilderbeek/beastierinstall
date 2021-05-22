55570f3c837e2c819f9be0323b7cf01f1a0e673c
richelbilderbeek <richel@richelbilderbeek.nl>
2021-05-22 11:21:09 +0200

    Add namespaces to test

--- tests/testthat/test-upgrade_beast2.R
+++ tests/testthat/test-upgrade_beast2.R
@@ -9,11 +9,13 @@ test_that("give error when BEAST2 is not installed", {
   if (!is_on_ci()) return()
   if (!is_beast2_installed()) return()
 
-  expect_true(is_beast2_installed())
+  expect_true(beastier::is_beast2_installed())
 
   beastierinstall::uninstall_beast2()
-  expect_error(upgrade_beast2(), "BEAST2 is not installed")
+  expect_error(
+    beastierinstall::upgrade_beast2(),
+    "BEAST2 is not installed"
+  )
   beastierinstall::install_beast2()
-
-  expect_true(is_beast2_installed())
+  expect_true(beastier::is_beast2_installed())
 })
