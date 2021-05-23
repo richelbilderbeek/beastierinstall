4aaa01ca02b996c1fec183bf06935a4669852217
richelbilderbeek <richel@richelbilderbeek.nl>
2021-05-23 12:31:22 +0200

    Add beastierinstall.R

---
+++ R/beastierinstall.R
@@ -0,0 +1,12 @@
+#' \code{beastierinstall}: A package to install BEAST2.
+#'
+#' \code{beastierinstall} allows to install and uninstall BEAST2,
+#' a popular Bayesian phylogenetics tool, using
+#' an R interface.
+#'
+#' \code{beastierinstall} in a non-CRAN extension of the
+#' \code{beastier} package. With \code{beastier} one can work with BEAST2
+#' using an R interface.
+#' @docType package
+#' @name beastierinstall
+NULL
