a68e96ea5fe9d2f1f987df22f947369a03f7cd90
richelbilderbeek <richel@richelbilderbeek.nl>
2021-07-03 09:44:05 +0200

    Fix test

--- tests/testthat/test-clear_beastier_cache.R
+++ tests/testthat/test-clear_beastier_cache.R
@@ -3,9 +3,10 @@ test_that("minimal use", {
 })
 
 test_that("create file", {
-  beautier_cache_folder_name <- dirname(get_beautier_tempfilename())
 
-  filename <- get_beautier_tempfilename()
+  beautier_cache_folder_name <- dirname(beautier::get_beautier_tempfilename())
+
+  filename <- beautier::get_beautier_tempfilename()
   dir.create(dirname(filename), showWarnings = FALSE, recursive = TRUE)
   readr::write_lines("irrelevant", file = filename)
   expect_equal(1, length(list.files(beautier_cache_folder_name)))
@@ -14,9 +15,9 @@ test_that("create file", {
 })
 
 test_that("create dir", {
-  beautier_cache_folder_name <- dirname(get_beautier_tempfilename())
+  beautier_cache_folder_name <- dirname(beautier::get_beautier_tempfilename())
 
-  folder_name <- get_beautier_tempfilename()
+  folder_name <- beautier::get_beautier_tempfilename()
   dir.create(folder_name, showWarnings = FALSE, recursive = TRUE)
   expect_equal(2, length(list.dirs(beautier_cache_folder_name)))
   expect_silent(clear_beautier_cache())
