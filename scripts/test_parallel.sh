80e96e0accddbc25dc87617da4528741035e97bd
richelbilderbeek <richel@richelbilderbeek.nl>
2023-10-06 18:15:52 +0200

    Fix bash scripts

--- scripts/test_parallel.sh
+++ scripts/test_parallel.sh
@@ -6,22 +6,10 @@
 echo "--------------"
 echo "Bash command: "
 echo "--------------"
-cmd='Rscript -e "beastier::do_minimal_run()"'
-echo $cmd
+cmd="Rscript -e \"beastier::do_minimal_run()\""
+echo "${cmd}"
 
 echo "--------------"
 echo "RUN"
 echo "--------------"
-(echo $cmd; echo $cmd; echo $cmd; echo $cmd; echo $cmd; echo $cmd; echo $cmd; echo $cmd; echo $cmd; echo $cmd; echo $cmd; echo $cmd; echo $cmd; echo $cmd; echo $cmd; echo $cmd; echo $cmd; echo $cmd; echo $cmd; echo $cmd; echo $cmd; echo $cmd; echo $cmd; echo $cmd; echo $cmd; echo $cmd; echo $cmd; echo $cmd; echo $cmd; echo $cmd) | parallel && echo "All jobs went OK"
-
-exit
-
-echo "--------------"
-echo "RUN 2"
-echo "--------------"
-for rng_seed in $(seq 1 2)
-do
-  $cmd
-done
-
-
+(echo "${cmd}"; echo "${cmd}"; echo "${cmd}"; echo "${cmd}"; echo "${cmd}"; echo "${cmd}"; echo "${cmd}"; echo "${cmd}"; echo "${cmd}"; echo "${cmd}"; echo "${cmd}"; echo "${cmd}"; echo "${cmd}"; echo "${cmd}"; echo "${cmd}"; echo "${cmd}"; echo "${cmd}"; echo "${cmd}"; echo "${cmd}"; echo "${cmd}"; echo "${cmd}"; echo "${cmd}"; echo "${cmd}"; echo "${cmd}"; echo "${cmd}"; echo "${cmd}"; echo "${cmd}"; echo "${cmd}"; echo "${cmd}"; echo "${cmd}") | parallel && echo "All jobs went OK"
